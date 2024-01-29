6d0bad210841d03332f57c6850f3a6c0a9ba090b
Justin Linton <127802772+aberrator9@users.noreply.github.com>
2024-01-28 20:26:27 -0500

    Add app page and initial styling.

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,19 @@
 <script setup>
+import Logo from './components/Logo.vue';
+
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-</header>
+  <header class="flex place-items-center">
+    <Logo class="flex-auto"></Logo>
+    <h1 class="flex-auto text-4xl font-bold text-cyan-50">PermaPin</h1>
+  </header>
+
+  <main class="flex place-items-center">
+    <div class="flex-wrap container place-items-start">
+      <label for="urls" class="text-cyan-50">Urls (separate with commas or new line):</label>
+      <textarea name="urls" class="flex-auto resize-none bg-cyan-50 font-medium text-lg m-4 w-96"></textarea>
+    </div>
+  </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
