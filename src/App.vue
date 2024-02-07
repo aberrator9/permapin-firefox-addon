5ff4f9e234264c2776492515ac83b79586a82773
Justin Linton <127802772+aberrator9@users.noreply.github.com>
2024-02-07 11:34:55 -0500

    Add 'Use currently pinned' tab url parsing.

--- src/App.vue
+++ src/App.vue
@@ -10,20 +10,22 @@
 
       <div class="flex flex-col gap-4 items-center">
         <label for="urls" class="text-cyan-50 text-lg">Urls (separate with commas or new line):</label>
-        <textarea
-          v-model="urls" @blur="parseUrls" name="urls"
+        <textarea v-model="urls" @blur="parseUrls()" name="urls"
           class="flex-auto resize-none border-cyan-600 border-2 bg-cyan-50 font-medium text-lg rounded-md w-96">
         </textarea>
       </div>
 
       <div class="flex justify-self-center justify-between max-w-96 mx-auto my-4">
-        <button @click="useCurrentlyPinned()" class="bg-cyan-900 hover:bg-cyan-700 active:bg-cyan-800 text-cyan-50 border-2 border-cyan-300 rounded-md px-2 py-1">Use currently pinned</button>
+        <button @click="useCurrentlyPinned()" class="bg-cyan-900 hover:bg-cyan-700 active:bg-cyan-800 text-cyan-50 border-2 border-cyan-300 rounded-md px-2 py-1">
+          Use currently pinned
+        </button>
         <div class="inline-flex flex-initial items-center gap-x-2">
-          <Input type="checkbox" v-model="pinToAllWindows" class="bg-cyan-900 text-cyan-50 border-2 border-cyan-300 rounded-md"></Input>
+          <Input type="checkbox" v-model="pinToAllWindows"
+            class="bg-cyan-900 text-cyan-50 border-2 border-cyan-300 rounded-md"></Input>
           <p class="text-cyan-50">Pin to all windows</p>
         </div>
       </div>
-      
+
     </main>
 
   </div>
@@ -44,12 +46,13 @@ const parseUrls = () => {
   console.log(tabs)
 }
 
-const useCurrentlyPinned = () => {
-  
-  const pinnedTabs = browser.tabs.query({ pinned: true }).then(tabs.forEach((t) => console.log(t)));;
-  // console.log(pinnedTabs)
+const getUrls = (tabs) => {
+  return tabs.map((t) => t.url).join('\n')
+}
 
-  // urls.value = pinnedTabs.value
+const useCurrentlyPinned = async () => {
+  urls.value = await browser.tabs.query({ pinned: true })
+    .then(getUrls)
 }
 
 </script>
