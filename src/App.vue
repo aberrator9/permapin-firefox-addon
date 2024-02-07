8769ee1411f9c4b1270547612b2a23219d5cc879
Justin Linton <127802772+aberrator9@users.noreply.github.com>
2024-02-07 11:00:30 -0500

    Add logos. Config vite to overwrite rather than delete inside /dist.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,3 @@
-<script setup>
-
-import { ref } from 'vue'
-import Logo from './components/Logo.vue'
-
-const tabsText = ref('')
-const tabs = []
-
-
-</script>
-
 <template>
   <div class="container">
 
@@ -18,20 +7,49 @@ const tabs = []
     </header>
 
     <main>
+
       <div class="flex flex-col gap-4 items-center">
         <label for="urls" class="text-cyan-50 text-lg">Urls (separate with commas or new line):</label>
-        <textarea v-bind="tabsText" @input="console.log(tabsText)" name="urls"
-          class="flex-auto resize-none border-cyan-600 border-2 bg-cyan-50 font-medium text-lg rounded-md w-96"></textarea>
+        <textarea
+          v-model="urls" @blur="parseUrls" name="urls"
+          class="flex-auto resize-none border-cyan-600 border-2 bg-cyan-50 font-medium text-lg rounded-md w-96">
+        </textarea>
       </div>
 
       <div class="flex justify-self-center justify-between max-w-96 mx-auto my-4">
-        <button class="bg-cyan-900 hover:bg-cyan-700 active:bg-cyan-800 text-cyan-50 border-2 border-cyan-300 rounded-md px-2 py-1">Use currently pinned</button>
+        <button @click="useCurrentlyPinned()" class="bg-cyan-900 hover:bg-cyan-700 active:bg-cyan-800 text-cyan-50 border-2 border-cyan-300 rounded-md px-2 py-1">Use currently pinned</button>
         <div class="inline-flex flex-initial items-center gap-x-2">
-          <Input type="checkbox" class="bg-cyan-900 text-cyan-50 border-2 border-cyan-300 rounded-md"></Input>
+          <Input type="checkbox" v-model="pinToAllWindows" class="bg-cyan-900 text-cyan-50 border-2 border-cyan-300 rounded-md"></Input>
           <p class="text-cyan-50">Pin to all windows</p>
         </div>
       </div>
+      
     </main>
 
   </div>
 </template>
+
+<script setup>
+
+import { ref } from 'vue'
+import Logo from './components/Logo.vue'
+
+const pinToAllWindows = ref(false)
+const urls = ref('')
+let tabs = []
+
+const parseUrls = () => {
+  tabs = urls.value.split(/[,\s]+/)
+  // TODO: Add validation for urls
+  console.log(tabs)
+}
+
+const useCurrentlyPinned = () => {
+  
+  const pinnedTabs = browser.tabs.query({ pinned: true }).then(tabs.forEach((t) => console.log(t)));;
+  // console.log(pinnedTabs)
+
+  // urls.value = pinnedTabs.value
+}
+
+</script>
